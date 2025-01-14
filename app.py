aeea2d033bf2c7faf7f5e206d9068a493eb90825
leetaemin <ltm7149@gmail.com>
2025-01-14 17:26:51 +0900

    streamlit_test

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-title = st.text_input("Movie title", "Life of Brian")
-st.write("The current movie title is", title)
---
+++ movie_poster.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import requests
+
+# TMDb API 키 설정
+API_KEY = 'd8008cc7b9a93e22bf785e9cda213ac0'  # 자신의 API 키로 교체
+
+# 영화 제목을 입력받는 텍스트 입력 필드
+movie_title = st.text_input("영화 제목을 입력하세요:")
+
+# 영화 제목이 입력되면 API를 호출
+if movie_title:
+    # TMDb API URL 설정
+    url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={movie_title}"
+
+    # API 호출하여 데이터 받기
+    response = requests.get(url)
+    data = response.json()
+    
+    # 영화 포스터 출력
+    if data['results']:
+        poster_path = data['results'][0]['poster_path']
+        poster_url = f"https://image.tmdb.org/t/p/w500{poster_path}"
+        st.image(poster_url, caption=f"영화: {data['results'][0]['title']}", use_container_width=True)
+    else:
+        st.error("영화를 찾을 수 없습니다.")
